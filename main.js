6a8697e5a02f60658b1dcfd668862976ac7a6142
Johann Jarmelo <johannjarmelo@gmail.com>
2026-03-31 11:55:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function abrirLinkedin() {
+  window.open("https://www.linkedin.com/in/johannjarmelo/", "_blank");
+}
+
+function abrirLinkedinContato() {
+  window.open("https://www.linkedin.com/in/johannjarmelo/overlay/contact-info/", "_blank");
+}
+
+function abrirProjeto(link) {
+  window.open(link, "_blank");
+}
